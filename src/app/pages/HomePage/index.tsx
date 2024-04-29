5a17811cc012b79e931bc6495f2c44f05cc3cc9c
S-FOOD\sohee.kang <sohee.kang@s-food.com>
2024-04-29 15:52:40 +0900

    useState로 Todo 일부분 기능 구현하기

--- src/app/pages/HomePage/index.tsx
+++ src/app/pages/HomePage/index.tsx
@@ -27,10 +27,27 @@ const Title = styled.h1`
 
 const TodoList = styled.div``;
 
-const TodoCheck = styled.input`
-  margin-right: 15px;
-`;
 export function HomePage() {
+  const [todoList, setTodoList] = React.useState<ITodoItem[]>([
+    {
+      id: '1',
+      content: '첫번째 투두',
+      completed: true,
+      editing: false,
+    },
+    {
+      id: '2',
+      content: '두번째 투두',
+      completed: false,
+      editing: false,
+    },
+    {
+      id: '3',
+      content: '세번째 투두',
+      completed: false,
+      editing: false,
+    },
+  ]);
   return (
     <>
       <Helmet>
@@ -40,32 +57,13 @@ export function HomePage() {
       <Wrapper>
         <Box>
           <Title>할 일</Title>
-          <TodoInput></TodoInput>
+          <TodoInput
+            setTodoList={(todo: ITodoItem) => setTodoList([todo, ...todoList])}
+          />
           <TodoList>
-            <TodoItem
-              todo={{
-                id: '1',
-                completed: false,
-                content: '투두 입니다 1',
-                editing: false,
-              }}
-            ></TodoItem>
-            <TodoItem
-              todo={{
-                id: '2',
-                completed: true,
-                content: '투두 입니다 2',
-                editing: false,
-              }}
-            ></TodoItem>
-            <TodoItem
-              todo={{
-                id: '3',
-                completed: true,
-                content: '투두 입니다 3',
-                editing: false,
-              }}
-            ></TodoItem>
+            {todoList.map(todo => (
+              <TodoItem todo={todo} />
+            ))}
           </TodoList>
         </Box>
       </Wrapper>
